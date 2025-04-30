1e1c3c444612cafeaae2e553ee99dc480ddbdf5e
Liam Boreback <boreback.work@gmail.com>
2025-04-30 16:18:08 +0200

    Add retry attempts for initial connections

--- src/server/initial_setup.rs
+++ src/server/initial_setup.rs
@@ -1,20 +1,23 @@
-use std::future;
+use std::{future, sync::Arc, time::Duration};
 
 use anyhow::Context;
-use futures::{stream::FuturesUnordered, StreamExt, TryStreamExt};
+use futures::{sink::Send, stream::FuturesUnordered, StreamExt, TryStreamExt};
+use hyper_util::{client::legacy::connect::HttpConnector, rt::TokioExecutor};
 use lib::{
     connection::ConnectionError,
+    custom_tls::DebugHasKey,
     key_storage::{KeyStorage, KeyStorageError},
-    keys::key_fingerprint_b64,
+    keys::{key_fingerprint_b64, CertificateData, HasKey},
     protocol::info::ServerInfo,
     HostPort,
 };
 use thiserror::Error;
+use tonic::transport::Channel;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("Error acquiring certificate: {0:?}")]
-    Aquiring(ConnectionError),
+    Aquiring(#[from] ConnectionError),
     #[error("Certificates were not matching")]
     NotMatching,
     #[error("Error retreiving server info: {0:?}")]
@@ -30,29 +33,47 @@ pub async fn contact_servers(
     servers: &[HostPort],
     key_store: &mut KeyStorage,
 ) -> Result<(), (HostPort, Error)> {
+    const RETRY_ATTEMPTS: u8 = 5;
+    const RETRY_DELAY: Duration = Duration::from_secs(5);
+
+    let connect_with_timeout = |(host, attempt, timeout)| async move {
+        tokio::time::sleep(timeout).await;
+        let cert = lib::connection::connect_and_get_cert(&host).await;
+        (host, attempt, cert)
+    };
+
     let mut certificate_futures = servers
         .into_iter()
-        .map(|host| async move {
-            let cert = lib::connection::connect_and_get_cert(&host).await;
-            (*host, cert)
-        })
+        .map(|host| (*host, 1, Duration::ZERO))
+        .map(connect_with_timeout)
         .collect::<FuturesUnordered<_>>();
 
     let info_futures = FuturesUnordered::new();
 
-    while let Some((host, cert_result)) = certificate_futures.next().await {
+    while let Some((host, attempt, cert_result)) = certificate_futures.next().await {
         let cert = match cert_result {
             Ok(cert) => cert,
+            Err(ConnectionError::Connection(e)) => {
+                if attempt > RETRY_ATTEMPTS {
+                    return Err((host, Error::Aquiring(ConnectionError::Connection(e))));
+                }
+
+                certificate_futures.push(connect_with_timeout((host, attempt + 1, RETRY_DELAY)));
+
+                continue;
+            }
             Err(e) => return Err((host, Error::Aquiring(e))),
         };
 
+        // Confirm fingerprint with user
         let fingerprint = key_fingerprint_b64(&*cert);
         if !confirm_fingerprint(&host.to_string(), &fingerprint).await {
             return Err((host, Error::NotMatching));
         }
 
+        // Add task to get the server info to queue
         info_futures.push(async move {
-            get_server_info(&host)
+            get_server_info(&host, &cert)
                 .await
                 .map(|info| (host.clone(), cert, info))
                 .map_err(|e| (host, Error::Info(e)))
@@ -93,12 +114,31 @@ async fn confirm_fingerprint(host: &str, fingerprint: &str) -> bool {
         }
     }
 }
+#[derive(Debug)]
+struct SingleKey(CertificateData);
+impl HasKey for SingleKey {
+    fn have_tonic_certificate(&self, cert: &tonic::transport::CertificateDer<'_>) -> bool {
+        *self.0.raw() == *cert.as_ref()
+    }
+}
+impl DebugHasKey for SingleKey {}
 
-async fn get_server_info(server: &HostPort) -> anyhow::Result<ServerInfo> {
+async fn get_server_info(server: &HostPort, cert: &CertificateData) -> anyhow::Result<ServerInfo> {
+    const CONNECTION_TIMEOUT: Duration = Duration::from_secs(5);
+
+    let single_key = Arc::new(SingleKey(*cert));
+
+    let tls = rustls::ClientConfig::builder()
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(
+            lib::custom_tls::CustomCertificateVerifier::new(single_key),
+        )).with_no_client_auth();
+
+    let mut http = HttpConnector::new();
+    http.enforce_http(false);
+
     let mut client =
-        lib::protocol::info::protocol::server_info_client::ServerInfoClient::connect(lib::to_endpoint(server)?)
-            .await
-            .context("Failed to connect to server")?;
+        lib::protocol::info::protocol::server_info_client::ServerInfoClient::with_origin(client, server.into());
 
     let response = client
         .get_server_info(tonic::Request::new(
