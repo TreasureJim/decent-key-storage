9d7309e59cb497614cd2f2e6d5eb748d8fc4e032
Liam Boreback <boreback.work@gmail.com>
2025-05-07 17:12:49 +0200

    Finish client

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,13 +1,14 @@
 #![feature(drain_keep_rest)]
+#![feature(let_chains)]
 
 mod connect_network;
 mod query_network;
 mod cross_ref;
 
 use anyhow::anyhow;
-use lib::{custom_tls::DebugHasKey, HostPort};
-use std::{path::PathBuf, sync::Arc};
-use tokio::sync::RwLock;
+use lib::HostPort;
+use uuid::Uuid;
+use std::path::PathBuf;
 
 use clap::Parser;
 
@@ -16,12 +17,16 @@ const DEFAULT_DATA_LOCATION: &str = "~/.local/decent-key-storage";
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
-    #[arg(long, value_name = "ADDR", value_parser = HostPort::parse_arg)]
-    server_addr: HostPort,
+    #[arg(long, value_name = "ADDR")]
+    server_uuid: Uuid,
     #[arg(long, value_name = "FOLDER", default_value = DEFAULT_DATA_LOCATION, value_parser = lib::key_storage::canonicalize_path)]
     data_folder: PathBuf,
     #[arg(long, value_name = "ADDRESSES", value_parser = HostPort::parse_arg)]
     connect_network: Vec<HostPort>,
+    #[arg(long, short)]
+    query_network: bool,
+    #[arg()]
+    n: usize
 }
 
 #[tokio::main]
@@ -47,43 +52,24 @@ async fn main() -> anyhow::Result<()> {
         return Err(anyhow!("ERROR: Network of nodes is not yet connected and connect-network argument not provided with 2 addresses."));
     }
 
-    let known_hosts = Arc::new(RwLock::new(known_hosts));
 
     // CASES:
+
     // Want to connect to a client we know - use key store
+    if !args.query_network && let Some(cert_data) = known_hosts.get_certificate_uuid(&args.server_uuid) {
+        println!("Found server locally:");
+        println!("{:?}", cert_data);
+        return Ok(());
+    }
+
     // Want to connect to a client we dont know - query many servers for its cert
-    // Want to connect to a client we know but the client contacted has a different cert:
-    //  - query other servers for what server is on this socket
-    //  - ask the server who it is
-    //  - obtain its certificate
-    //  - check if info is correct -- PASS or FAIL
+    query_network::query_for_uuid(&mut known_hosts, &args.server_uuid, args.n).await?;
 
-    let client = lib::connection::safe_client(known_hosts.read().await.snapshot());
-    let mut share_certs_client =
-        lib::protocol::proto::share_cert::cert_sharing_client::CertSharingClient::with_origin(
-            client,
-            (&args.server_addr).into(),
-        );
-
-    let certs = share_certs_client
-        .get_certificates(tonic::Request::new(
-            lib::protocol::proto::share_cert::RequestCertificates {
-                uuids: vec![],
-            },
-        ))
-        .await?
-        .into_inner();
-
-    // TRUST ON FIRST USE MODEL
-
-    // let mut client = greeter_client::new(channel);
-    // let request = tonic::Request::new(EchoRequest {
-    //     message: "hello".into(),
-    // });
-
-    // let response = client.unary_echo(request).await?;
-    //
-    // println!("RESPONSE={:?}", response);
+    if let Some(cert_data) = known_hosts.get_certificate_uuid(&args.server_uuid) {
+        println!("Retrieved server:");
+        println!("{:?}", cert_data);
+        return Ok(());
+    }
 
     Ok(())
 }
