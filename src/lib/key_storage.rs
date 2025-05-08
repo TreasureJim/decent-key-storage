7b2423e9a9052243714c1e80d6960e22fdcdb9f2
Liam Boreback <boreback.work@gmail.com>
2025-05-08 12:32:49 +0200

    Move bimap to its own module

--- src/lib/key_storage.rs
+++ src/lib/key_storage.rs
@@ -1,6 +1,9 @@
+mod bimap;
+
 use anyhow::Context;
 use arc_swap::ArcSwap;
 use base64::Engine;
+use bimap::UuidCertBiMap;
 use path_absolutize::Absolutize;
 use pkcs8::der::pem::PemLabel;
 use pkcs8::der::{Encode, EncodePem};
@@ -31,49 +34,6 @@ use crate::keys::{CertWithMetadata, CertificateData, HasKey, Identity, NodeInfo}
 pub fn canonicalize_path(path: &str) -> Result<PathBuf, anyhow::Error> {
     let expanded = expanduser::expanduser(path)?;
     Ok(expanded.absolutize()?.into_owned())
-}
-
-
-#[derive(Debug)]
-struct UuidCertBiMap {
-    uuid_to_cert: HashMap<Uuid, Arc<CertificateData>>,
-    cert_to_uuid: HashMap<Arc<CertificateData>, Uuid>,
-}
-
-impl UuidCertBiMap {
-    pub fn new() -> Self {
-        Self {
-            uuid_to_cert: HashMap::new(),
-            cert_to_uuid: HashMap::new(),
-        }
-    }
-
-    pub fn get_cert(&self, uuid: &Uuid) -> Option<&Arc<CertificateData>> {
-        self.uuid_to_cert.get(uuid)
-    }
-
-    pub fn get_uuid(&self, cert: &CertificateData) -> Option<&Uuid> {
-        self.cert_to_uuid.get(cert)
-    }
-
-    /// Returns true if map already contained certicate
-    pub fn insert(&mut self, uuid: Uuid, cert: CertificateData)  {
-        let rc = Arc::new(cert);
-        self.uuid_to_cert.insert(uuid, rc.clone());
-        self.cert_to_uuid.insert(rc, uuid);
-    }
-
-    pub fn get_all_certificates(&self) -> Vec<&Arc<CertificateData>> {
-        self.cert_to_uuid.keys().collect()
-    }
-
-    pub fn get_all_uuids(&self) -> Vec<&Uuid> {
-        self.uuid_to_cert.keys().collect()
-    }
-
-    pub fn iter(&self) -> std::collections::hash_map::Iter<'_, Uuid, Arc<CertificateData>> {
-        self.uuid_to_cert.iter()
-    }
 }
 
 
---
+++ src/lib/key_storage/bimap.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+pub struct UuidCertBiMap {
+    uuid_to_cert: HashMap<Uuid, Arc<CertificateData>>,
+    cert_to_uuid: HashMap<Arc<CertificateData>, Uuid>,
+}
+
+impl UuidCertBiMap {
+    pub fn new() -> Self {
+        Self {
+            uuid_to_cert: HashMap::new(),
+            cert_to_uuid: HashMap::new(),
+        }
+    }
+
+    pub fn get_cert(&self, uuid: &Uuid) -> Option<&Arc<CertificateData>> {
+        self.uuid_to_cert.get(uuid)
+    }
+
+    pub fn get_uuid(&self, cert: &CertificateData) -> Option<&Uuid> {
+        self.cert_to_uuid.get(cert)
+    }
+
+    /// Returns true if map already contained certicate
+    pub fn insert(&mut self, uuid: Uuid, cert: CertificateData)  {
+        let rc = Arc::new(cert);
+        self.uuid_to_cert.insert(uuid, rc.clone());
+        self.cert_to_uuid.insert(rc, uuid);
+    }
+
+    pub fn get_all_certificates(&self) -> Vec<&Arc<CertificateData>> {
+        self.cert_to_uuid.keys().collect()
+    }
+
+    pub fn get_all_uuids(&self) -> Vec<&Uuid> {
+        self.uuid_to_cert.keys().collect()
+    }
+
+    pub fn iter(&self) -> std::collections::hash_map::Iter<'_, Uuid, Arc<CertificateData>> {
+        self.uuid_to_cert.iter()
+    }
+}
