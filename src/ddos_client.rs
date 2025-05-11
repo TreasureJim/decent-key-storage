0abda037c3846fb6878ce956c9f87038e8180d65
Liam Boreback <boreback.work@gmail.com>
2025-05-11 03:03:55 +0200

    Add max time limit and only log non error handle returns

--- src/ddos_client.rs
+++ src/ddos_client.rs
@@ -22,12 +22,15 @@ use tokio::{
 };
 use tokio_stream::StreamExt;
 use tokio_util::sync::CancellationToken;
+use tonic::Code;
 
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
+    #[arg(long, value_parser = parse_duration)]
+    poll_time: Option<Duration>,
     #[arg()]
     server: SocketAddr,
     #[arg()]
@@ -42,6 +45,11 @@ struct Args {
     process_args: Vec<String>,
 }
 
+fn parse_duration(arg: &str) -> Result<std::time::Duration, std::num::ParseIntError> {
+    let seconds = arg.parse()?;
+    Ok(std::time::Duration::from_secs(seconds))
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let env = env_logger::Env::default().default_filter_or("info");
@@ -53,16 +61,27 @@ async fn main() -> anyhow::Result<()> {
     info!("Spawning: {} {:?}", args.process, args.process_args);
     let mut child = Command::new(&args.process)
         .args(&args.process_args)
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
+        // .stdout(Stdio::null())
+        // .stderr(Stdio::null())
         .spawn()
         .expect("Failed to spawn child process");
     let pid = child.id();
     info!("Child PID: {}", pid);
 
+    let uri: Uri = HostPort::new(args.server).into();
+    // Test network
+    {
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        let resp = unsafe_query_network_for_uuid(uri.clone()).await.context("Failed to make test connection");
+        if resp.is_err() {
+            child.kill()?;
+            resp?;
+        }
+    }
+
     let stop = CancellationToken::new();
 
-    let perf_file = args.output.join("performance.csv");
+    let perf_file = args.output.join(format!("performance_{}.csv", args.hz));
     let perf_writer = Arc::new(Mutex::new(csv::Writer::from_path(perf_file)?));
     let writer_clone = perf_writer.clone();
     let stop_clone = stop.clone();
@@ -74,14 +93,12 @@ async fn main() -> anyhow::Result<()> {
     info!("Making {} requests per second", args.hz);
     let (handle_tx, handle_rx) = tokio::sync::mpsc::unbounded_channel();
 
-    let responses_file = args.output.join("responses.csv");
+    let responses_file = args.output.join(format!("responses_{}.csv", args.hz));
     let responses_writer = Arc::new(Mutex::new(csv::Writer::from_path(responses_file)?));
     let writer_clone = responses_writer.clone();
     let stop_clone = stop.clone();
     let rec_handle =
         tokio::spawn(async move { log_handles(handle_rx, writer_clone, stop_clone).await });
-
-    let uri: Uri = HostPort::new(args.server).into();
 
     select! {
         err = ddos(args.hz, uri, handle_tx) => {
@@ -95,6 +112,9 @@ async fn main() -> anyhow::Result<()> {
         }
         _ = tokio::signal::ctrl_c() => {
             log::info!("Received shutdown signal");
+        }
+        _ = tokio::time::sleep(args.poll_time.unwrap()), if args.poll_time.is_some() => {
+            log::info!("Timer finished");
         }
     };
 
@@ -114,13 +134,24 @@ async fn main() -> anyhow::Result<()> {
 async fn ddos(
     hz: usize,
     uri: Uri,
-    handle_tx: UnboundedSender<JoinHandle<()>>,
+    handle_tx: UnboundedSender<JoinHandle<bool>>,
 ) -> anyhow::Result<()> {
     let duration = Duration::from_millis((1000.0 / hz as f64) as u64);
     loop {
         let uri = uri.clone();
         let handle = tokio::spawn(async move {
-            unsafe_query_network_for_uuid(uri).await;
+            let response = unsafe_query_network_for_uuid(uri).await;
+            if let Err(e) = response {
+                match e.code() {
+                    Code::Unavailable | Code::Internal | Code::DataLoss => {
+                        return false;
+                    }
+                    _ => {
+                        return true;
+                    }
+                }
+            }
+            true
         });
         handle_tx.send(handle).context("Sending handle")?;
 
@@ -130,27 +161,30 @@ async fn ddos(
 
 /// Connects to `n` multiple nodes in the network and queries them for a UUID's cert
 /// Then cross references
-async fn unsafe_query_network_for_uuid(uri: Uri) {
+async fn unsafe_query_network_for_uuid(
+    uri: Uri,
+) -> Result<tonic::Response<lib::protocol::proto::share_cert::ResponseCertificates>, tonic::Status>
+{
     let client = lib::connection::dangerous_client();
     let mut client =
         lib::protocol::proto::share_cert::cert_sharing_client::CertSharingClient::with_origin(
             client, uri,
         );
 
-    let _ = client
+    client
         .get_certificates(tonic::Request::new(
             lib::protocol::proto::share_cert::RequestCertificates { uuids: vec![] },
         ))
-        .await;
+        .await
 }
 
 async fn log_handles(
-    mut handle_rx: UnboundedReceiver<JoinHandle<()>>,
+    mut handle_rx: UnboundedReceiver<JoinHandle<bool>>,
     writer: Arc<Mutex<csv::Writer<File>>>,
     stop: CancellationToken,
 ) -> anyhow::Result<()> {
     let mut writer = writer.lock().await;
-    writer.write_record(&["received_at".to_string()])?;
+    writer.write_record(&["received_at".to_string(), "success".to_string()])?;
 
     let mut futures = futures::stream::futures_unordered::FuturesUnordered::new();
 
@@ -162,9 +196,14 @@ async fn log_handles(
                 }
             }
             finished = futures.next() => {
-                if let Some(_) = finished {
-                    writer.write_record(&[Utc::now().to_rfc3339()])?;
-                    let _ = writer.flush();
+                match finished {
+                    Some(Ok(true)) => {
+                        writer.write_record(&[Utc::now().to_rfc3339(), true.to_string()])?;
+                    }
+                    Some(Ok(false)) => {
+                        writer.write_record(&[Utc::now().to_rfc3339(), false.to_string()])?;
+                    }
+                    _ => {}
                 }
             }
             _ = stop.cancelled() => {
@@ -205,7 +244,6 @@ async fn log_performance(
             let mem = proc.memory();
 
             writer.write_record(&[ts, format!("{:.2}", cpu), mem.to_string()])?;
-            let _ = writer.flush();
         } else {
             info!("Process {} not found (maybe exited)", pid);
             break;
